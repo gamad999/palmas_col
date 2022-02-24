cb149f3406754e2c85bfc6e619546709032f4492
Geovanny <geoconsultores.eu@gmail.com>
2022-02-24 18:55:45 -0500

    Segmentación geografica de registros y especies

--- palmas.sql
+++ palmas.sql
@@ -1,18 +1,30 @@
 CREATE EXTENSION postgis;
 
 -- Creación de campos de enlace espacial por departamentos, municipios y ecoregiones
-ALTER TABLE arecaceae ADD COLUMN departamento varchar(40);
+
 ALTER TABLE arecaceae ADD COLUMN depto varchar(80);
 ALTER TABLE arecaceae ADD COLUMN municipio varchar(70);
 ALTER TABLE arecaceae ADD COLUMN ecoregion varchar(80);
+ALTER TABLE arecaceae ADD COLUMN parquenac varchar(80);
 
 -- Aplicación de funciones de enlace espacial
 
+-- Segmentación de registros y especies de palmas por departamento
 UPDATE arecaceae SET depto = dpto_cnmbr
 FROM departamentos
 WHERE ST_Intersects(arecaceae.geom, departamentos.geom);
 
--- Primera consulta espacial de numero de especies de palmas por departamento
-SELECT depto as departamento, COUNT(DISTINCT species) as especies
-FROM arecaceae
-GROUP BY depto ORDER BY especies DESC;
+-- Segmentación de registros y especies por municipio
+UPDATE arecaceae SET municipio = mpio_cnmbr
+FROM municipios
+WHERE ST_Intersects(arecaceae.geom, municipios.geom);
+
+--Segmentación de registros y especies por ecorregión de WWF
+UPDATE arecaceae SET ecoregion = eco_name
+FROM ecoregiones
+WHERE ST_Intersects(arecaceae.geom, ecoregiones.geom);
+
+-- Segmentación geografica por area protegida (presencia o ausencia)
+UPDATE arecaceae SET parquenac = nombre
+FROM parquenacional
+WHERE ST_Intersects(arecaceae.geom, parquenacional.geom);
